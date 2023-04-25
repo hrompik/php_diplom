6e3d2e6b4ea37bb5045e18cc97228207d5e3cacd
hrompik <hrompi@ya.ru>
2023-04-25 12:49:22 +0300

    Добавил страницы контакты и о нас и ссылки на них

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,8 +11,18 @@ class MainController extends AbstractController
     #[Route('/', name: 'app_main')]
     public function index(): Response
     {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
-        ]);
+        return $this->render('main/index.html.twig');
+    }
+
+    #[Route('/contacts', name: 'app_contacts')]
+    public function contacts(): Response
+    {
+        return $this->render('main/contacts.html.twig');
+    }
+
+    #[Route('/about', name: 'app_about')]
+    public function about(): Response
+    {
+        return $this->render('main/about.html.twig');
     }
 }
